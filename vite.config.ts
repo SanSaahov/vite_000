d4ddb303d8e2e7c52dc2eb45f6a316c2a02afe18
SanSaahov <soyzov@gmail.com>
2025-04-07 22:17:56 +0300

    commit #02 changes vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig ( {
+   plugins: [
+      vue (),
+   ],
+   base: '/vite_000/'
+} )
